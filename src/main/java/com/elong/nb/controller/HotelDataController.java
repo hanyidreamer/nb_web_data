2f8da81c68b9ede949d034cddad8f89a4a89b6d5
yingquan.li <yingquan.li@corp.elong.com>
2016-09-05 16:12:52 +0800

    拆分库存和价格的controller代码

--- src/main/java/com/elong/nb/controller/HotelDataController.java
+++ src/main/java/com/elong/nb/controller/HotelDataController.java
@@ -43,78 +43,12 @@ public class HotelDataController {
 			.getProperty("HasNoPrepayAuth.restRequest.Request");
 	private String unKownException=CommonsUtil.CONFIG_PROVIDAR
 			.getProperty("UnKownException.restRequest.Request");
-
-	@Resource
-	private IRateService RateService;
 	@Resource
 	private IRatePlansService ratePlansService;
 	@Resource
 	private IBookingDataService bookingDataService;
 	@Resource
 	private IRackRateService rackRateService;
-	
-	@RequestMapping(value = "/api/Hotel/GetInventories", method = RequestMethod.POST)
-	public ResponseEntity<byte[]> getInventories(HttpServletRequest request)
-	{
-		//RestResponse<InventoryResult> response = new RestResponse<InventoryResult>(
-		//		"");
-//		Object restRequest;
-//		GetInventoryChangeDetailRequest obj=new GetInventoryChangeDetailRequest();
-//		obj.setHotelID("50101624");
-//		obj.setBeginTime(new DateTime(2016,8,18,0,0));
-//		obj.setEndTime(new DateTime(2016,8,20,0,0));
-//		GetInventoryChangeDetailResponse oi=ProductForPartnerServiceContract.getInventoryChangeDetail(obj);
-		return new ResponseEntity(GsonUtil.toJson(null,
-				0).getBytes(), HttpStatus.OK);
-		
-	}
-	@SuppressWarnings("rawtypes")
-	@RequestMapping(value = "/api/Hotel/GetRates", method = RequestMethod.POST)
-	public ResponseEntity<byte[]> getRates(HttpServletRequest request) throws IOException
-	{
-		Map<Class, TypeAdapter> m = new HashMap<Class, TypeAdapter>();
-		RestRequest<RateCondition> restRequest = GsonUtil.toReq(request,
-				RateCondition.class, m);
-		String rst = validateRateRequest(restRequest);
-		if (StringUtils.isNotBlank(rst)) {
-			RestResponse<RateResult> response = new RestResponse<RateResult>(
-					restRequest.getGuid());
-			response.setCode(rst);
-			return new ResponseEntity(GsonUtil.toJson(response,
-					restRequest.getVersion() == null ? 0d : restRequest.getVersion()).getBytes(), HttpStatus.OK);
-		}
-		RestResponse<RateResult> response=null;
-		try {
-			response = RateService.getRates(restRequest);
-		} catch (Exception e) {
-			response=new RestResponse<RateResult>(restRequest.getGuid());
-			response.setCode(unKownException+e.getMessage());
-		}
-		return new ResponseEntity(GsonUtil.toJson(
-				response,
-				restRequest.getVersion()).getBytes(), HttpStatus.OK);
-		
-		
-	}
-	
-	private String validateRateRequest(RestRequest<RateCondition> restRequest){
-		;
-		StringBuffer sb = new StringBuffer(
-				ValidateUtil.validateRestRequest(restRequest));
-		RateCondition req = restRequest.getRequest();
-		/**
-		 * 如果没有预付权限还查询，则返回错误信息
-		 */
-		if (req.getPaymentType() == EnumPaymentType.Prepay
-				&& (!restRequest.getProxyInfo().getEnabledPrepayProducts())) {
-			if (req.getPaymentType() == EnumPaymentType.Prepay) {
-				sb.append(hasNoPrepayAuth);
-				return sb.toString();
-			} 
-		}
-		return sb.toString();
-	}
-	
 	//获取产品
 	@SuppressWarnings("rawtypes")
 	@RequestMapping(value = "/api/Hotel/GetRatePlans", method = RequestMethod.POST)
