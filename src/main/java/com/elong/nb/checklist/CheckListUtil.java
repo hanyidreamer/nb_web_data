0f688c4ad2146e99018cb857ffcb683efc2bf25d
yingquan.li <yingquan.li@corp.elong.com>
2017-05-09 11:02:20 +0800

    细化业务逻辑

--- src/main/java/com/elong/nb/checklist/CheckListUtil.java
+++ src/main/java/com/elong/nb/checklist/CheckListUtil.java
@@ -7,8 +7,12 @@ import com.elong.springmvc_enhance.utilities.ActionLogHelper;
 
 public class CheckListUtil {
 	public static void info(BigLog bigLog){
+		int responseCode=0;
+		if(!StringUtils.isEmpty(bigLog.getBusinessErrorCode())){
+			responseCode=Integer.valueOf(bigLog.getBusinessErrorCode());
+		}
 		ActionLogHelper.businessLog(bigLog.getTraceId(), true, bigLog.getServiceName(),
-				bigLog.getAppName(),null, StringUtils.isEmpty(bigLog.getElapsedTime())?0:Integer.valueOf(bigLog.getElapsedTime()), 0, null, bigLog.getResponseBody(),bigLog.getRequestBody());
+				bigLog.getAppName(),null, StringUtils.isEmpty(bigLog.getElapsedTime())?0:Integer.valueOf(bigLog.getElapsedTime()), responseCode, bigLog.getExceptionMsg(), bigLog.getResponseBody(),bigLog.getRequestBody());
 	}
 	public static void error(BigLog bigLog){
 		ActionLogHelper.businessLog(bigLog.getTraceId(), false, bigLog.getServiceName(),
